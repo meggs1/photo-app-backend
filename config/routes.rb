103f1446d47b6565e69bd3effdeb199885b65698
megan <megan.schertz@gmail.com>
2022-03-11 16:23:22 -0600

    add posts routes back

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,10 @@ Rails.application.routes.draw do
   post '/logout',   to: 'sessions#destroy'
   get '/logged_in', to: 'sessions#is_logged_in?'
 
+  
   resources :users, only: [:create, :show, :index] do 
     resources :items, only: [:create, :show, :index, :destroy]
   end
+  resources :posts
   
 end
